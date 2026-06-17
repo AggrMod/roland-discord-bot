f9662bcebb67a430a8f330ed2b520612d1e8033b
roland-cmyk <roland@kleermaker.online>
2026-06-17 15:40:57 +0200

    Fix ReferenceError in applyTenantModuleNavVisibility causing redirect to landing page

--- web/public/portal.js
+++ web/public/portal.js
@@ -2184,7 +2184,7 @@ function applyTenantModuleNavVisibility(settings = {}) {
     nfttracker: !!settings.moduleNftTrackerEnabled,
     tokentracker: !!settings.moduleTokenTrackerEnabled,
     aiassistant: !!settings.moduleAiAssistantEnabled,
-    telegrambridge: !!(modules?.telegrambridge?.enabled || settings.moduleTelegramBridgeEnabled),
+    telegrambridge: !!settings.moduleTelegramBridgeEnabled,
     heist: !!settings.moduleMissionsEnabled,
     vault: !!settings.moduleVaultEnabled,
     welcome: settings.moduleWelcomeEnabled !== false,
